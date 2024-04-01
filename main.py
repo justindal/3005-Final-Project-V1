e260fde333095dabf9ecce69b35b090a53308035
justindal <justindaludado@cmail.carleton.ca>
2024-04-01 03:38:37 -0400

    work on populating database

--- main.py
+++ main.py
@@ -58,8 +58,6 @@ def get_relevant_data() -> []:
     return [competition_ids, season_ids, match_ids, match_file_paths, event_file_paths, lineup_file_paths]
 
 
-
-
 # data from competitions.json
 def populate_from_competitions(cursor: psycopg.cursor):
     with open('open-data/data/competitions.json') as f:
@@ -70,19 +68,6 @@ def populate_from_competitions(cursor: psycopg.cursor):
                                                                                                  '2018/2019']
             c2 = competition['competition_name'] == 'Premier League' and competition['season_name'] == '2003/2004'
             if c1 or c2:
-                # countries
-                cursor.execute(
-                    'INSERT INTO countries (country_name) VALUES (%s)',
-                    (competition['country_name'],)
-                )
-
-                # get country_id
-                cursor.execute(
-                    'SELECT country_id FROM countries WHERE country_name = %s',
-                    (competition['country_name'],)
-                )
-                country_id = cursor.fetchone()[0]
-
                 # seasons
                 cursor.execute(
                     'INSERT INTO season (competition_id, season_id, season_name) VALUES (%s, %s, %s)',
@@ -92,21 +77,64 @@ def populate_from_competitions(cursor: psycopg.cursor):
                 # competitions
                 cursor.execute(
                     'INSERT INTO competition (competition_id, competition_name, competition_gender, '
-                    'competition_youth, competition_international, country_id, season_id) VALUES (%s, %s, %s, %s, %s, '
-                    '%s, %s)',
-                    (competition['competition_id'], competition['competition_name'], competition['competition_gender'],
-                     competition['competition_youth'], competition['competition_international'], country_id,
-                     competition['season_id'])
+                    'competition_youth, competition_international, season_id, country_name)'
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                    (
+                        competition['competition_id'], competition['competition_name'],
+                        competition['competition_gender'],
+                        competition['competition_youth'], competition['competition_international'],
+                        competition['season_id'], competition['country_name'])
                 )
 
-# data from matches folder
+
+# data from matches folder TODO not finished
 def populate_from_matches(cursor: psycopg.cursor, match_file_paths: []):
     for file in match_file_paths:
         with open(file) as f:
             data = json.load(f)
             for match in data:
-                pass
+                # add teams to database
+                cursor.execute(
+                    'INSERT INTO team (team_id, team_name, team_gender, team_group, country_id) '
+                    'VALUES (%s, %s, %s, %s, %s)',
+                    (
+                        match['home_team']['home_team_id'], match['home_team']['home_team_name'],
+                        match['home_team']['home_team_gender'], match['home_team']['home_team_group'],
+                        match['home_team']['country']['id'])
+                )
 
+                # add match to database
+                referee_id = match['referee']['id'] if 'referee' in match else None
+                cursor.execute(
+                    'INSERT INTO match (match_id, match_date, kick_off, home_score, away_score, match_week, '
+                    'competition_id, home_team_id, away_team_id, competition_stage_id, stadium_id, referee_id, '
+                    'home_manager_id, away_manager_id, season_id)'
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                    (
+                        match['match_id'],
+                        match['match_date'],
+                        match['kick_off'],
+                        match['home_score'],
+                        match['away_score'],
+                        match['match_week'],
+                        match['competition']['competition_id'],
+                        match['home_team']['home_team_id'],
+                        match['away_team']['away_team_id'],
+                        match['competition_stage']['id'],
+                        match['stadium']['id'],
+                        referee_id,
+                        match['home_team']['managers'][0]['id'],
+                        match['away_team']['managers'][0]['id'],
+                        match['season']['season_id']
+                    )
+                )
+
+
+def create_tables(cursor: psycopg.cursor):
+    # get file named createTables.sql
+    with open('createTables.sql', 'r') as file:
+        sql = file.read()
+        cursor.execute(sql)
 
 
 def main():
@@ -115,21 +143,16 @@ def main():
         dbname='PROJECT'
     )
     cursor = connection.cursor()
+
     data = get_relevant_data()
     # createTables(cursor)
     # populate_from_competitions(cursor)
-
+    populate_from_matches(cursor, data[3])
+    # print(data[3][0])
 
     connection.commit()
     cursor.close()
     connection.close()
-
-
-def create_tables(cursor: psycopg.cursor):
-    # get file named createTables.sql
-    with open('createTables.sql', 'r') as file:
-        sql = file.read()
-        cursor.execute(sql)
 
 
 if __name__ == '__main__':
