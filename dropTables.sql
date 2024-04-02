86694b63deaaa7db1a0104cb76c5da5e09ab89a0
justindal <justindaludado@cmail.carleton.ca>
2024-04-02 14:40:41 -0400

    fix dropping tables with dependents

--- dropTables.sql
+++ dropTables.sql
@@ -1,43 +1,23 @@
+drop table lineup_player cascade;
+drop table tactics cascade;
+
 drop table competition;
-
 drop table player_position;
-
 drop table pass;
-
 drop table pass_height;
-
 drop table pass_body_part;
-
 drop table pass_type;
-
 drop table play_pattern;
-
-drop table tactics;
-
 drop table match_event;
-
 drop table position;
-
 drop table event_type;
-
-drop table lineup_player;
-
 drop table lineup;
-
 drop table match;
-
 drop table season;
-
 drop table team;
-
 drop table manager;
-
 drop table referee;
-
 drop table stadium;
-
 drop table competition_stage;
-
 drop table player;
-
 drop table countries;
