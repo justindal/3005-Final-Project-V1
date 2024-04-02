af2e03c2a9a98e153afd7e72dfd5ef188d730eff
justindal <justindaludado@cmail.carleton.ca>
2024-04-01 21:58:23 -0400

    drop tables command added

---
+++ dropTables.sql
@@ -0,0 +1,43 @@
+drop table competition;
+
+drop table player_position;
+
+drop table pass;
+
+drop table pass_height;
+
+drop table pass_body_part;
+
+drop table pass_type;
+
+drop table play_pattern;
+
+drop table tactics;
+
+drop table match_event;
+
+drop table position;
+
+drop table event_type;
+
+drop table lineup_player;
+
+drop table lineup;
+
+drop table match;
+
+drop table season;
+
+drop table team;
+
+drop table manager;
+
+drop table referee;
+
+drop table stadium;
+
+drop table competition_stage;
+
+drop table player;
+
+drop table countries;
